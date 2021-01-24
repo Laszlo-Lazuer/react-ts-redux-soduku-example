5fca546ab703948b63787e670ff974280a2d0b07
Laszlo Lazuer <visualjazz@gmail.com>
2021-01-24 17:49:02 -0600

    chore: adds components

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 import { ThemeProvider } from 'styled-components'
+import { Content, Title } from './components'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello World</div>
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 )
