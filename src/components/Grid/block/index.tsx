04f90172ceb159c850fbfb34b5cd214390cc3ce0
Laszlo Lazuer <visualjazz@gmail.com>
2021-01-24 18:30:51 -0600

    chore: adds unique keys to grid blocks

--- src/components/Grid/block/index.tsx
+++ src/components/Grid/block/index.tsx
@@ -6,7 +6,7 @@ const Block: FC = () => {
   return (
     <div data-cy="grid-container">
       {[...Array(9)].map((_, rowIndex) => (
-        <div data-cy="grid-row-container">
+        <div data-cy="grid-row-container" key={rowIndex}>
           {[...Array(9)].map((_, colIndex) => (
             <div data-cy="block"></div>
           ))}
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom'
 import { unregister } from './core'
 import { GlobalStyles, theme } from './styles'
 import { ThemeProvider } from 'styled-components'
-import { Content, Title, Card } from './components'
+import { Content, Title, Card, Grid } from './components'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
     <Content data-cy="content">
       <Title data-cy="title">Sudoku</Title>
-      <Card data-cy="card">This is a work in progress</Card>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
     </Content>
   </ThemeProvider>,
   document.getElementById('root')
